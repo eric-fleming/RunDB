316fd0ea1f6026e301357222f7c9a95c2172b78f
mrfleming <ericfleming8@gmail.com>
2020-07-28 12:08:10 -0500

    refactored into routes folder

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const port = 3001;
 const runners = require('./routes/runner.routes');
 const runs = require('./routes/run.routes');
 
-// DATABASE QUERIES
-const runner_db_queries = require('./database/run_model');
-const running_history_db_queries = require('./database/running_history_db_queries');
-
-
 
 
 app.use(express.json());
@@ -42,94 +37,6 @@ app.get('/info', (req, res) => {
 app.get('/error', (req, res) => {
     res.sendFile('./public/error.html', { root: __dirname });
 });
-
-
-/* app.get('/runs', (req, res) => {
-    running_history_db_queries.getRunningHistory()
-        .then(response => {
-            res.status(200).send({ data: response});
-            console.log(`Fetching runs...`);
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        })
-}); */
-
-/* app.get('/runners', (req, res) => {
-    runner_db_queries.getAllRunners()
-        .then(response => {
-            res.status(200).send({ data: response });
-            console.log(`Fetching runners...`);
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        })
-});
-
-app.get('/runners/:id', (req, res) => {
-    runner_db_queries.getRunnerById(req.params.id)
-        .then(response => {
-            res.status(200).send({ data: response });
-            console.log(`Fetching runner by id:${req.params.id}...`);
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        })
-});
-
-
-app.post('/addrunner', (req,res) => {
-    
-    runner_db_queries.createRunner(req.body)
-    .then(response => {
-        res.status(200).send(response);
-        console.log(`Processing POST : ${req.body.first} ${req.body.last}`);
-    })
-    .catch(error => {
-        res.status(500).send(error)
-        console.log(`ERROR POST : ${req.body.first} ${req.body.last}`);
-    })
-});
-
-app.put('/runners/:id', (req,res)=>{
-    runner_db_queries.editRunner(req.body)
-        .then(response => {
-            res.status(200).send(response);
-            console.log(`Processing PUT : ${req.body.first} ${req.body.last}`);
-        })
-        .catch(error => {
-            res.status(500).send(error)
-            console.log(`ERROR PUT : ${req.body.first} ${req.body.last}`);
-        })
-});
-
-app.delete('/runners/:id',(req,res)=>{
-    
-    runner_db_queries.deleteRunner(req.params.id)
-        .then(response => {
-            res.status(200).send(response);
-            console.log(`Processing DELETE : ID=${req.params.id}`);
-        })
-        .catch(error => {
-            res.status(500).send(error)
-            console.log(`ERROR DELETE : ID=${req.params.id}`);
-            console.log(error)
-        })
-}); */
-
-/* app.post('/addrun', (req, res) => {
-
-    running_history_db_queries.createRun(req.body)
-        .then(response => {
-            res.status(200).send(response);
-            console.log(`Processing POST : runner=${req.body.runnerid} dist=${req.body.distance} time=${req.body.time}`);
-        })
-        .catch(error => {
-            res.status(500).send(error)
-            console.log(`ERROR POST : runner=${req.body.runnerid} dist=${req.body.distance} time=${req.body.time}`);
-        })
-}); */
-
 
 
 // START SERVER
