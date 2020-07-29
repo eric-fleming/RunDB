022db4251e3248e6ff56496636261b8f16a16511
mrfleming <ericfleming8@gmail.com>
2020-07-29 12:42:20 -0500

    added ability to update and delete runs

--- database/running_history_db_queries.js
+++ database/running_history_db_queries.js
@@ -12,7 +12,7 @@ const pool = new Pool({
 
 const getRunningHistory = () => {
     return new Promise(function (resolve, reject) {
-        pool.query('SELECT * FROM public."History"', (error, results) => {
+        pool.query('SELECT * FROM public."RunHistory"', (error, results) => {
             if (error) {
                 reject(error);
             }
@@ -26,7 +26,7 @@ const createRun = (body) => {
     return new Promise(function (resolve, reject) {
         const { runnerid, date, distance, time } = body;
 
-        let db_command = 'INSERT INTO public."History" (date,distance,runnerid,time) VALUES ($1,$2,$3,$4)';
+        let db_command = 'INSERT INTO public."RunHistory" (date,distance,runnerid,time) VALUES ($1,$2,$3,$4)';
         pool.query(db_command, [date, distance, runnerid, time], (error, results) => {
             if (error) {
                 console.log(error)
@@ -42,8 +42,43 @@ const createRun = (body) => {
 };
 
 
+
+const editRun = (body) => {
+    return new Promise(function (resolve, reject) {
+        const { run_uid, date, distance, time } = body;
+
+        let db_command = 'UPDATE public."RunHistory" SET date = $1, distance = $2, time= $3 WHERE run_uid=$4';
+        pool.query(db_command, [date, distance, time, run_uid], (error, results) => {
+            if (error) {
+                console.log(error)
+                reject(error)
+            }
+            resolve(`Run modified to: ${results.rows[0]}`)
+
+        });
+    });
+};
+
+
+const deleteRun = (id) => {
+    return new Promise(function (resolve, reject) {
+        const runUID = parseInt(id);
+        let db_command = 'DELETE FROM public."RunHistory" WHERE run_uid=$1';
+        pool.query(db_command, [runUID], (error, results) => {
+            if (error) {
+                reject(error)
+            }
+            resolve(`Run with run_uid:${runUID} was deleted.`)
+        });
+    });
+};
+
+
+
 // EXPORTS
 module.exports = {
     getRunningHistory,
-    createRun
+    createRun,
+    editRun,
+    deleteRun
 };
--- routes/run.routes.js
+++ routes/run.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const running_history_db_queries = require('../database/running_history_db_queries');
+const run_table = require('../database/running_history_db_queries');
 // middleware that logs time
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
@@ -9,7 +9,7 @@ router.use(function timeLog(req, res, next) {
 
 // Base Route : /runs
 router.get('/', (req, res) => {
-    running_history_db_queries.getRunningHistory()
+    run_table.getRunningHistory()
         .then(response => {
             res.status(200).send({ data: response });
             console.log(`Fetching runs...`);
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
 
-    running_history_db_queries.createRun(req.body)
+    run_table.createRun(req.body)
         .then(response => {
             res.status(200).send(response);
             console.log(`Processing POST : runner=${req.body.runnerid} dist=${req.body.distance} time=${req.body.time}`);
@@ -29,6 +29,32 @@ router.post('/', (req, res) => {
         .catch(error => {
             res.status(500).send(error)
             console.log(`ERROR POST : runner=${req.body.runnerid} dist=${req.body.distance} time=${req.body.time}`);
+        })
+});
+
+router.put('/:run_uid', (req, res) => {
+    run_table.editRun(req.body)
+        .then(response => {
+            res.status(200).send(response);
+            console.log(`Processing PUT : ${req.body}`);
+        })
+        .catch(error => {
+            res.status(500).send(error)
+            console.log(`ERROR PUT : ${req.body}`);
+        })
+});
+
+router.delete('/:run_uid', (req, res) => {
+    console.log(req.params);
+    run_table.deleteRun(req.params.run_uid)
+        .then(response => {
+            res.status(200).send(response);
+            console.log(`Processing DELETE : ID=${req.params.run_uid}`);
+        })
+        .catch(error => {
+            res.status(500).send(error)
+            console.log(`ERROR DELETE : ID=${req.params.run_uid}`);
+            console.log(error)
         })
 });
 
