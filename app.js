e5c3e90c61d812c11e858f9f9d24e2db5fbc9c6b
mrfleming <ericfleming8@gmail.com>
2020-08-12 16:05:59 -0500

    allows * cors over http://localhost:5000

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const mongo = require('./routes/mongo.routes');
 app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
     next();
